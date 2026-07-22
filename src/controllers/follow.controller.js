b067396e078d83002cdf4a0ba5e1452543e66f0f
meethatkar <meethatkar000@gmail.com>
2026-07-22 20:57:16 +0530

    feat: add getFollowCount endpoint and improve follow/unfollow logic

--- src/controllers/follow.controller.js
+++ src/controllers/follow.controller.js
@@ -2,131 +2,153 @@ const followModel = require("../models/follow.model");
 const userModel = require("../models/auth.model");
 
 async function followUserController(req, res) {
-    const followerName = req.user.username;
-    const followeeName = req.params.username;
+  const followerName = req.user.username;
+  const followeeName = req.params.username;
 
-    // 1st Check:- can't follow yourself
-    if (followeeName == followerName) {
-        return res.status(409).json({
-            message: "you can't follow yourself"
-        })
-    }
-
-    // 2nd Check:-  checking user (followee) exists or not ?
-    const isFolloweeExists = await userModel.findOne({
-        username: followeeName
+  // 1st Check:- can't follow yourself
+  if (followeeName == followerName) {
+    return res.status(409).json({
+      message: "you can't follow yourself",
     });
+  }
 
-    if (!isFolloweeExists) {
-        return res.status(404).json({
-            message: "user not exists"
-        })
-    }
+  // 2nd Check:-  checking user (followee) exists or not ?
+  const isFolloweeExists = await userModel.findOne({
+    username: followeeName,
+  });
 
-    // 3rd Check:- checking does user already followed it or not.
-    const isAlreadyFollowed = await followModel.findOne({
-        follower: followerName,
-        followee: followeeName
-    })
+  if (!isFolloweeExists) {
+    return res.status(404).json({
+      message: "user not exists",
+    });
+  }
 
-    if (isAlreadyFollowed) {
-        return res.status(409).json({
-            message: `you are already following ${followeeName}`
-        })
-    }
+  // 3rd Check:- checking does user already followed it or not.
+  const isAlreadyFollowed = await followModel.findOne({
+    follower: followerName,
+    followee: followeeName,
+  });
 
-    const followingData = await followModel.create({
-        follower: followerName,
-        followee: followeeName,
-        status: "pending"
-    })
+  if (isAlreadyFollowed) {
+    return res.status(409).json({
+      message: `you are already following ${followeeName}`,
+    });
+  }
 
-    res.status(201).json({
-        message: `${followerName} now follows ${followeeName}`,
-        data: followingData
-    })
+  const followingData = await followModel.create({
+    follower: followerName,
+    followee: followeeName,
+    status: "pending",
+  });
+
+  res.status(201).json({
+    message: `${followerName} now follows ${followeeName}`,
+    data: followingData,
+  });
 }
 
 async function unfollowUserController(req, res) {
-    const followerName = req.user.username;
-    const followeeName = req.params.username;
+  const followerName = req.user.username;
+  const followeeName = req.params.username;
 
-    const isFolloweeExists = await userModel.findOne({
-        username: followeeName
+  const isFolloweeExists = await userModel.findOne({
+    username: followeeName,
+  });
+
+  if (!isFolloweeExists) {
+    return res.status(404).json({
+      message: "user not exists",
     });
+  }
 
-    if (!isFolloweeExists) {
-        return res.status(404).json({
-            message: "user not exists"
-        })
-    }
+  const isUserBeginFollowed = await followModel.findOne({
+    follower: followerName,
+    followee: followeeName,
+  });
 
-    const isUserBeginFollowed = await followModel.findOne({
-        follower: followerName,
-        followee: followeeName
-    })
+  if (!isUserBeginFollowed) {
+    return res.status(404).json({
+      message: `you are not following ${followeeName} user`,
+    });
+  }
 
-    if (!isUserBeginFollowed) {
-        return res.status(404).json({
-            message: `you are not following ${followeeName} user`
-        })
-    }
+  const data = await followModel.findByIdAndDelete({
+    _id: isUserBeginFollowed._id,
+  });
 
-    const data = await followModel.findByIdAndDelete({
-        _id: isUserBeginFollowed._id
-    })
-
-    res.status(200).json({
-        message: `you are now not following ${followeeName} user now`
-    })
+  res.status(200).json({
+    message: `you are now not following ${followeeName} user now`,
+  });
 }
 
 async function updateFollowStatusController(req, res) {
-    const followerName = req.user.username;
-    const followeeName = req.params.username;
-    const { status } = req.body;
+  const followerName = req.user.username;
+  const followeeName = req.params.username;
+  const { status } = req.body;
 
-    const isFolloweeExists = await userModel.findOne({
-        username: followeeName
+  const isFolloweeExists = await userModel.findOne({
+    username: followeeName,
+  });
+
+  if (!isFolloweeExists) {
+    return res.status(404).json({
+      message: "followee not exists",
     });
+  }
 
-    if (!isFolloweeExists) {
-        return res.status(404).json({
-            message: "followee not exists"
-        })
-    }
+  const isFolloweeFollows = await followModel.findOne({
+    followee: followeeName,
+    follower: followerName,
+  });
 
-    const isFolloweeFollows = await followModel.findOne({
-        followee: followeeName,
-        follower: followerName
-    })
+  if (!isFolloweeFollows) {
+    return res.status(404).json({
+      message: `${followeeName} not follows ${followerName}`,
+    });
+  }
 
-    if (!isFolloweeFollows) {
-        return res.status(404).json({
-            message: `${followeeName} not follows ${followerName}`
-        })
-    }
+  const updatedStatus = await followModel.findByIdAndUpdate(
+    isFolloweeFollows._id,
+    {
+      status: status,
+    },
+    {
+      runValidators: true,
+      returnDocument: "after",
+    },
+  );
 
-    const updatedStatus = await followModel.findByIdAndUpdate(
-        isFolloweeFollows._id,
-        {
-            status: status
-        },
-        {
-            runValidators: true,
-            returnDocument: "after"
-        }
-    )
+  res.status(200).json({
+    message: "status updated",
+    data: updatedStatus,
+    new: true,
+  });
+}
 
-    res.status(200).json({
-        message: "status updated",
-        data: updatedStatus,
-        new: true
-    })
+async function getFollowCount(req, res) {
+  const { username } = req.params;
+
+  const followeeCount = await followModel.countDocuments({
+    followee: username,
+  });
+
+  const followerCount = await followModel.countDocuments({
+    follower: username,
+  });
+
+  console.log(followerCount);
+  console.log(followeeCount);
+
+  res.status(200).json({
+    message: "fetched following and follower count",
+    followeeCount,
+    followerCount,
+  });
 }
 
 module.exports = {
-    followUserController,
-    unfollowUserController,
-    updateFollowStatusController
-}
+  followUserController,
+  unfollowUserController,
+  updateFollowStatusController,
+  getFollowCount,
+};
--- src/routes/follow.routes.js
+++ src/routes/follow.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const identifyUser = require("../middlewares/auth.middleware");
-const followController = require("../controllers/follow.controller")
+const followController = require("../controllers/follow.controller");
 
 const followRoute = express.Router();
 
@@ -8,22 +8,45 @@ const followRoute = express.Router();
  * @route POST /api/users/follow/:username
  * @description follow a user
  * @access Private
-*/
-followRoute.post("/follow/:username", identifyUser, followController.followUserController)
+ */
+followRoute.post(
+  "/follow/:username",
+  identifyUser,
+  followController.followUserController,
+);
 
 /**
  * @route POST /api/users/unfollow/:username
  * @description  unfollow a user
  * @access Private
-*/
-followRoute.post("/unfollow/:username", identifyUser, followController.unfollowUserController)
+ */
+followRoute.post(
+  "/unfollow/:username",
+  identifyUser,
+  followController.unfollowUserController,
+);
 
 /**
  * @route POST /api/users/status/:username
  * @description update follow status
  * @access Private
  */
-followRoute.post("/status/:username", identifyUser, followController.updateFollowStatusController);
+followRoute.post(
+  "/status/:username",
+  identifyUser,
+  followController.updateFollowStatusController,
+);
+
+/**
+ * @route GET /api/users/count/:username
+ * @description get followers and following count
+ * @access Private
+ */
+followRoute.get(
+  "/count/:username",
+  identifyUser,
+  followController.getFollowCount,
+);
 
 // TODO:- API To get list of followers and following
 
